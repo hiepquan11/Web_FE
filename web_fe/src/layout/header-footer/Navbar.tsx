e1d591361ddcb1a482a73376f51e03e95baa15f2
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-08-14 13:59:49 +0700

    Funtion: Update function search

--- web_fe/src/layout/header-footer/Navbar.tsx
+++ web_fe/src/layout/header-footer/Navbar.tsx
@@ -24,9 +24,6 @@ function NavBar({searchTerm, setSearchTerm}: NavbarProps){
   
     return (
      
-
-
-
       <nav className="bg-yellow-300 border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -47,7 +44,9 @@ function NavBar({searchTerm, setSearchTerm}: NavbarProps){
               </svg>
               <span className="sr-only">Search icon</span>
           </div>
-          <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
+          <form onSubmit={handleSubmit}>
+          <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={tempKeyWord} onChange={handleInputChange} placeholder="Search..."/>
+          </form>
           </div>
           <div className="pl-5 flex justify-center">
               <button>
