2cd221f93cfab59a2a6d1eabb8ba683b1139ac9d
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-08-15 13:30:15 +0700

    UI: Update UI Product Detail

--- web_fe/src/layout/Product/ProductDeatail.tsx
+++ web_fe/src/layout/Product/ProductDeatail.tsx
@@ -51,9 +51,34 @@ function ProductDetail(){
     }
 
     return(
-        <div>
-            <h1>{product?.ProductName}</h1>
+        <div className='flex flex-col justify-between lg:flex-row gap-16 lg:items-center p-4'>
+        <div className='flex flex-col gap-6 lg:w-2/4 items-center'>
+            <img src="" alt="" className='max-w-[300px] max-h-[300px] w-full h-full aspect-square object-cover rounded-xl'/>
+            <div className='flex flex-row justify-between h-24'>
+                <img src="" alt="" className='w-24 h-24 rounded-md cursor-pointer'/>
+                <img src="" alt="" className='w-24 h-24 rounded-md cursor-pointer' />
+                <img src="" alt="" className='w-24 h-24 rounded-md cursor-pointer' />
+                <img src="" alt="" className='w-24 h-24 rounded-md cursor-pointer'/>
+            </div>
         </div>
+        {/* ABOUT */}
+        <div className='flex flex-col gap-4 lg:w-2/4'>
+            <div>
+                <span className=' text-violet-600 font-semibold'>Special Sneaker</span>
+                <h1 className='text-3xl font-bold'>{product?.ProductName}</h1>
+            </div>
+            <p className='text-gray-700'>{product?.Description}</p>
+            <h6 className='text-2xl font-semibold'>{product?.Price.toLocaleString('vi-VN')} &#8363;</h6>
+            <div className='flex flex-row items-center gap-12'>
+                <div className='flex flex-row items-center'>
+                    <button className='bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl'>-</button>
+                    <span className='py-4 px-6 rounded-lg'></span>
+                    <button className='bg-gray-200 py-2 px-4 rounded-lg text-violet-800 text-3xl'>+</button>
+                </div>
+                <button className='bg-yellow-300 text-black font-semibold py-3 px-16 rounded-xl h-full'>Add to Cart</button>
+            </div>
+        </div>
+    </div>
     );
 }
 export default ProductDetail;
