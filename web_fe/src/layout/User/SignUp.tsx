b2768435a4a736993dcfe423a254e8ea2be22c9d
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-08-19 14:31:35 +0700

    Function: Update function select province and district

--- web_fe/src/layout/User/SignUp.tsx
+++ web_fe/src/layout/User/SignUp.tsx
@@ -1,20 +1,75 @@
+import { SetStateAction, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Select from 'react-select';
+import Select, { SingleValue } from 'react-select';
+import UserModel from "../../Models/UserModel";
 
+interface Province{
+    value: string;
+    label: string;
+}
+
+interface District{
+    value: string;
+    label: string;
+}
 
 function SignUp(){
 
-    const provinces = [
-        {
-            value: 'province1', label:'Provice1'
-        }
-    ]
+    const [provinces, setProvinces] = useState<Province[]>([]);
+    const [selectedProvince, setSelectedProvince] = useState<string | null>(null);
+    const [districts, setDistricts] = useState<District[]>([]);
+    const [selectedDistrict, setSelectedDistrict] = useState<string | null>(null);
+    const [user, setUser] = useState<UserModel>();
 
-    const districts = [
-        {
-            value:'district1', label:'District1'
+    useEffect(() =>{
+        async function fetchProvince() {
+            try {
+                const response = await fetch('https://vapi.vnappmob.com/api/province/');
+                const data = await response.json();
+
+                const formattedProvince: Province[] = data.results.map((province: any) => ({
+                    value: province.province_id,
+                    label: province.province_name
+                }));
+
+                setProvinces(formattedProvince);
+            } catch (error) {
+                console.log("error fetching province: ", error);
+            }
         }
-    ]
+        fetchProvince();
+    },[])
+
+    useEffect(() =>{
+        async function fetchDistrict() {
+            try {
+                const response = await fetch(`https://vapi.vnappmob.com/api/province/district/${selectedProvince}`);
+                const data = await response.json();
+                
+                const formattedDistrict: District[] = data.results.map((district: any) =>({
+                    value: district.district_id,
+                    label: district.district_name
+                }));
+                setDistricts(formattedDistrict)
+            } catch (error) {
+                console.log("error fetching district: ", error)
+            }
+        }
+        if(selectedProvince){
+            fetchDistrict();
+            setSelectedDistrict(null);
+        }
+    },[selectedProvince])
+
+    const handleSelectedProvince = (selectedID: SingleValue<Province>) =>{
+        setSelectedProvince(selectedID ? selectedID.value : null);
+        console.log(selectedProvince);
+    }
+
+    const handleSelectedDistrict = (selectedID: SingleValue<District>) =>{
+        setSelectedDistrict(selectedID ? selectedID.value : null);
+    }
+
     return(
         <div className="max-w-xl md:mx-auto mx-5 bg-white shadow-2xl rounded-lg h-auto md:mt-10 mt-36 mb-10 py-5">
 
@@ -32,9 +87,9 @@ function SignUp(){
             <input type="tel" name="phonenumber" placeholder="Số điện thoại" className="w-full py-3 rounded-md shadow-2xl mb-6 pl-5"/>
            
             <div className="flex space-x-4 mb-6">
-                <Select options={provinces} className="w-1/2" placeholder="Tỉnh/Thành phố" />
+                <Select options={provinces} className="w-1/2" placeholder="Tỉnh/Thành phố" onChange={handleSelectedProvince}/>
 
-                <Select options={districts} className="w-1/2" placeholder="Quận/Huyện" />
+                <Select options={districts} className="w-1/2" placeholder="Quận/Huyện" onChange={handleSelectedDistrict} value={districts.find(d => d.value === selectedDistrict) || null} />
             </div>
 
 
