f19d74850d95d3551b059e8512437bf0a337b195
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-08-23 14:19:30 +0700

    Function: Update api province, district, ward

--- web_fe/src/layout/User/SignUp.tsx
+++ web_fe/src/layout/User/SignUp.tsx
@@ -12,12 +12,19 @@ interface District{
     label: string;
 }
 
+interface Ward{
+    value: string;
+    lable: string;
+}
+
 function SignUp(){
 
     const [provinces, setProvinces] = useState<Province[]>([]);
     const [selectedProvince, setSelectedProvince] = useState<string | null>(null);
     const [districts, setDistricts] = useState<District[]>([]);
     const [selectedDistrict, setSelectedDistrict] = useState<string | null>(null);
+    const [wards, setWards] = useState<Ward[]>([]);
+    const [selectedWard, setSelectedWard] = useState<string | null>(null);
     const [user, setUser] = useState({
         fullname:'',
         username:'',
@@ -25,6 +32,7 @@ function SignUp(){
         phonenumber:'',
         province: '',
         district: '',
+        ward:'',
         password:'',
         confirmpassword:''
     })
@@ -42,7 +50,7 @@ function SignUp(){
        const checkUsername = await checkExistUsername(user.username);
        const checkEmail = await checkExistEmail(user.email);
 
-        if (!user.fullname || !user.username || !user.email || !user.phonenumber || !user.province || !user.district || !user.password || !user.confirmpassword) {
+        if (!user.fullname || !user.username || !user.email || !user.phonenumber || !user.province || !user.district || !user.ward || !user.password || !user.confirmpassword) {
             error = "Vui lòng điền đầy đủ thông tin";
             setNotify(null);
         } else if (checkUsername) {
@@ -80,7 +88,7 @@ function SignUp(){
                         phoneNumber: user.phonenumber,
                         userName: user.username,
                         email: user.email,
-                        address: `${user.province},${user.district}`,
+                        address: `${user.province},${user.district},${user.ward}`,
                         password: user.password
                     })
                 });
@@ -132,12 +140,12 @@ function SignUp(){
     useEffect(() =>{
         async function fetchProvince() {
             try {
-                const response = await fetch('https://vapi.vnappmob.com/api/province/');
+                const response = await fetch('https://esgoo.net/api-tinhthanh/1/0.htm');
                 const data = await response.json();
 
-                const formattedProvince: Province[] = data.results.map((province: any) => ({
-                    value: province.province_id,
-                    label: province.province_name
+                const formattedProvince: Province[] = data.data.map((province: any) => ({
+                    value: province.id,
+                    label: province.name
                 }));
 
                 setProvinces(formattedProvince);
@@ -151,12 +159,12 @@ function SignUp(){
     useEffect(() =>{
         async function fetchDistrict() {
             try {
-                const response = await fetch(`https://vapi.vnappmob.com/api/province/district/${selectedProvince}`);
+                const response = await fetch(`https://esgoo.net/api-tinhthanh/2/${selectedProvince}.htm`);
                 const data = await response.json();
                 
-                const formattedDistrict: District[] = data.results.map((district: any) =>({
-                    value: district.district_id,
-                    label: district.district_name
+                const formattedDistrict: District[] = data.data.map((district: any) =>({
+                    value: district.id,
+                    label: district.full_name
                 }));
                 setDistricts(formattedDistrict)
             } catch (error) {
@@ -169,6 +177,28 @@ function SignUp(){
         }
     },[selectedProvince])
 
+    useEffect(() =>{
+        async function fetchWard() {
+            try {
+                const response = await fetch(`https://esgoo.net/api-tinhthanh/3/${selectedDistrict}.htm`);
+                const data = await response.json();
+
+                const formattedWard: Ward[] = data.data.map((ward: any) =>({
+                    value: ward.id,
+                    label: ward.full_name
+                }));
+                setWards(formattedWard);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        if(selectedDistrict || selectedProvince){
+            fetchWard();
+            setSelectedWard(null);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[selectedDistrict])
+
     const handleSelectedProvince = (selectedID: SingleValue<Province>) =>{
         setSelectedProvince(selectedID ? selectedID.value : null);
         setUser({...user, province: selectedID ? selectedID.label : ''});
@@ -178,9 +208,13 @@ function SignUp(){
         setSelectedDistrict(selectedID ? selectedID.value : null);
         setUser({...user, district: selectedID ? selectedID.label : ''})
     }
+    const handleSelectedWard = (selectedID: SingleValue<Ward>) =>{
+        setSelectedWard(selectedID ? selectedID.value: null);
+        setUser({...user, ward: selectedID ? selectedID.lable : ''})
+    }
 
     return(
-        <div className="max-w-xl md:mx-auto mx-5 bg-white shadow-2xl rounded-lg h-auto md:mt-10 mt-36 mb-10 py-5">
+        <div className="max-w-2xl md:mx-auto mx-5 bg-white shadow-2xl rounded-lg h-auto md:mt-10 mt-36 mb-10 py-5">
             <div className="text-center">
                 <h1 className="text-lg font-semibold tracking-wide mb-2">ĐĂNG KÝ</h1>
             </div>
@@ -200,6 +234,8 @@ function SignUp(){
                 <div className="flex space-x-4 mb-6">
                     <Select options={provinces} className="w-1/2" placeholder="Tỉnh/Thành phố" onChange={handleSelectedProvince} />
                     <Select options={districts} className="w-1/2" placeholder="Quận/Huyện" onChange={handleSelectedDistrict} value={districts.find(d => d.value === selectedDistrict) || null} />
+                    <Select options={wards} className="w-1/2" placeholder="Phường/xã" onChange={handleSelectedWard} value={wards.find(w => w.value === selectedWard) || null} />
+
                 </div>
 
                 <input type="password" name="password" placeholder="Mật khẩu" className="w-full py-3 rounded-md shadow-2xl mb-6 pl-5" onChange={handleChange}/>
