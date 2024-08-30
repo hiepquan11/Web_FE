b60f2ce75c145de58f9004908f93a14ba7c4c200
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-08-30 16:51:20 +0700

    Function: Complete funtion login

--- web_fe/src/layout/User/SignIn.tsx
+++ web_fe/src/layout/User/SignIn.tsx
@@ -1,17 +1,56 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SignIn(){
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async (event: React.FormEvent) =>{
+
+        event.preventDefault();
+
+        const loginRequest = {
+            userName: username,
+            password: password
+        }
+        
+        try {
+            const response = await fetch('http://localhost:8080/userAccount/login',{
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(loginRequest)
+            });
+
+            if(!response.ok){
+                throw new Error("Đăng nhập thất bại");
+            }
+
+
+            const data = await response.json();
+            const {jwt} = data;
+            localStorage.setItem('token',jwt);
+            setError("Đăng nhập thành công");
+            
+        } catch (error) {
+            setError("Tài khoản hoặc mật khẩu không chính xác");
+        }
+       
+    }
+
+    console.log(username, password);
+    
     return(
         <div className="max-w-2xl md:mx-auto mx-5 bg-white shadow-2xl rounded-lg h-auto md:mt-10 mt-36 mb-10 py-5">
             <div className="text-center">
                 <h1 className="text-lg font-semibold tracking-wide mb-2">ĐĂNG NHẬP</h1>
             </div>
-
-            
-
-            <form className="text-center mx-20">
-                <input type="text" name="username" placeholder="Tên tài khoản" className="w-full py-3 rounded-md shadow-2xl mb-6 pl-5"/>
-                <input type="text" name="password" placeholder="Mật khẩu" className="w-full py-3 rounded-md shadow-2xl mb-6 pl-5"/>
+            <form className="text-center mx-20" onSubmit={handleLogin}>
+                <input type="text" name="username" placeholder="Tên tài khoản" className="w-full py-3 rounded-md shadow-2xl mb-6 pl-5" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <input type="password" name="password" placeholder="Mật khẩu" className="w-full py-3 rounded-md shadow-2xl mb-6 pl-5" value={password} onChange={(e) => setPassword(e.target.value)}/>
 
                 <button className="text-sm font-semibold bg-blue-600 py-2 px-3 rounded-md text-white flex items-center">
                     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -21,8 +60,12 @@ function SignIn(){
                 </button>
 
                 <br/>
+                {error && <div className="text-red-500">
+                        {error}
+                    </div>}
 
                 <button type="submit" className="text-sm font-semibold bg-pink-600 py-2 px-3 rounded-md text-white hover:bg-indigo-600 mb-4">Đăng nhập</button>
+                
             
                 
                 <h1 className="">Bạn chưa có tài khoản? <Link to={"/signup"} className="text-sm font-medium text-indigo-600">Đăng ký</Link></h1>
