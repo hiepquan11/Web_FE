aafce8250e4568d71da23f9c0949f3025c640c41
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-09-01 20:16:35 +0700

    UI: update admin dashboard

---
+++ web_fe/src/Api/admin/Dashboard.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "./Components/Sidebar";
+
+function Dashboard(){
+    return(
+        <div className="text-gray-800 font-inter">
+            <Sidebar/>
+        </div>
+    )
+}
+export default Dashboard;
--- web_fe/src/App.tsx
+++ web_fe/src/App.tsx
@@ -3,23 +3,22 @@ import "./App.css";
 import HomePage from "./layout/Home/HomePage";
 import NavBar from "./layout/header-footer/NavBar";
 import Footer from "./layout/header-footer/Footers";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import About from "./layout/about/About";
 import ProductDetail from "./layout/Product/ProductDeatail";
 import SignUp from "./layout/User/SignUp";
 import SignIn from "./layout/User/SignIn";
 import ActivationPage from "./layout/User/ActiovationPage";
+import Dashboard from "./Api/admin/Dashboard";
 
 
-
-function App() {
-  
+const MyRoute = () =>{
   const [searchTerm, setSearchTerm] = useState('');
-
-  return (
-   <div className="App">
-      <BrowserRouter>
-        <NavBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}></NavBar>
+  const location = useLocation();
+  const isAdmin = location.pathname.startsWith('/admin');
+  return(
+    <>
+       {!isAdmin && <NavBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}></NavBar>}
         <Routes>
           <Route path="/" element = {<HomePage searchTerm={searchTerm}/>}></Route>
           <Route path="/about" element = {<About/>}/>
@@ -27,8 +26,22 @@ function App() {
           <Route path="/signin" element={<SignIn/>}/>
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="/activate/:email/:activationCode" element={<ActivationPage/>}/>
+          <Route path="/admin" element={<Dashboard/>}/>
         </Routes>
-        <Footer/>
+        {!isAdmin && <Footer/>}
+    </>
+  )
+}
+
+
+function App() {
+  
+  
+
+  return (
+   <div className="App">
+      <BrowserRouter>
+        <MyRoute/>
       </BrowserRouter>
    </div>
   );
