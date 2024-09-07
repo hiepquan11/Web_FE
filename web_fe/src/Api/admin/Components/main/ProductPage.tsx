efcfdea7d0e92836ce87e21ba4fa6e1fefdf70c7
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-09-07 12:41:29 +0700

    Update UI Admin Product

--- web_fe/src/Api/admin/Components/main/ProductPage.tsx
+++ web_fe/src/Api/admin/Components/main/ProductPage.tsx
@@ -4,6 +4,7 @@ import ImageModel from "../../../../Models/ImageModel";
 import { GetAllProduct } from "../../../ProductApi";
 import CategoryModel from "../../../../Models/CategoryModel";
 import { getCategoryByProductID } from "../../../CategoryApi";
+import { Button } from "flowbite-react";
 
 const ProductPage = () =>{
     const [listProduct, setListProduct] = useState<ProductModel[]>([]);
@@ -32,15 +33,27 @@ const ProductPage = () =>{
             Danh sách sản phẩm
         </div>
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg ml-64">
-                <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
-                    <label  className="sr-only">Search</label>
-                    <div className="relative">
-                        <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                        </div>
-                        <input type="text" id="table-search" className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items"/>
+        <div className="flex flex-col sm:flex-row flex-wrap items-center justify-between pb-4">
+            <div className="flex items-center space-x-4">
+                <label className="sr-only">Search</label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                        <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path>
+                        </svg>
                     </div>
+                    <input
+                        type="text"
+                        id="table-search"
+                        className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Search for items"
+                    />
                 </div>
+            </div>
+            <div className="flex justify-center items-center mt-4 sm:mt-0">
+                <Button className="bg-green-500 hover:bg-green-950">Thêm sản phẩm mới</Button>
+            </div>
+        </div>
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -85,13 +98,15 @@ const ProductPage = () =>{
                             <td className="px-6 py-4">
                                 {product.Price.toLocaleString('vi-VN')}đ
                             </td>
-                            <td className="px-6 py-4">
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            <td className="px-6 py-4 flex gap-2">
+                                <Button className="w-1/3 bg-yellow-300 font-medium items-center">Chỉnh sửa</Button>
+                                <Button className="w-1/3 bg-red-700 font-medium items-center">Xóa</Button>
                             </td>
                         </tr>
                         ))}
                     </tbody>
                 </table>
+               
             </div>
         </>
     )
